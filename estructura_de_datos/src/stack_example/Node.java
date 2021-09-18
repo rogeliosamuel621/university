8f357b12d38fdfe98eca0e59ea7d910b4b255664
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-18 14:18:39 -0600

    Node class for stack structure added

---
+++ estructura_de_datos/src/stack_example/Node.java
@@ -0,0 +1,24 @@
+package stack_example;
+
+public class Node {
+    Node prev;
+    int data;
+
+    public void Node() {
+        this.prev = null;
+        this.data = 0;
+    }
+
+    public void Node(Node prev, int data) {
+        this.prev = prev;
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+}
