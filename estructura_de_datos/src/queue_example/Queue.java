b92bdf47e05b5ca86d3dce367b35a8d8f8891dd5
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-21 20:29:59 -0600

    insert and remove method added to queue

---
+++ estructura_de_datos/src/queue_example/Queue.java
@@ -0,0 +1,61 @@
+package queue_example;
+
+public class Queue {
+    Node first;
+    Node last;
+    int capacity;
+    int length;
+
+    private boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    private boolean isFully() {
+        return this.length == this.capacity;
+    }
+
+    public void insert(int value) {
+        Node node = new Node(value);
+
+        if (this.isFully()) {
+            System.out.println("La cola esta llena");
+            return;
+        }
+
+        if (this.isEmpty()) {
+            this.first = node;
+            this.last = node;
+            this.upgradeLength();
+            return;
+        }
+
+        this.last.prev = node;
+        this.last = node;
+        this.upgradeLength();
+    }
+
+    public void remove() {
+        if (this.isEmpty()) {
+            System.out.println("La cola esta vacia");
+            return;
+        }
+
+        if (this.length == 1) {
+            this.first = null;
+            this.last = null;
+            this.downgradeLength();
+            return;
+        }
+
+        this.first = this.first.prev;
+        this.downgradeLength();
+    }
+
+    private void upgradeLength() {
+        this.length++;
+    }
+
+    private void downgradeLength() {
+        this.length--;
+    }
+}
