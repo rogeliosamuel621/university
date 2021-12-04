e0cb25c6897ec7d713fa1a935e1ad7e40c5175bc
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-03 23:22:45 -0700

    librería

--- estructura_de_datos/src/evidencia_2/App.java
+++ estructura_de_datos/src/evidencia_2/App.java
@@ -1,7 +1,21 @@
 package evidencia_2;
 
+import java.util.TreeMap;
+
 public class App {
     public static void main(String[] args) {
+        TreeMap<Integer, String> binaryTree = new TreeMap<>();
+
+        binaryTree.put(1, "1");
+        binaryTree.put(2, "2");
+        binaryTree.put(3, "3");
+        binaryTree.put(4, "4");
+        binaryTree.put(5, "5");
+        binaryTree.put(6, "6");
+        binaryTree.put(7, "7");
+        binaryTree.put(8, "8");
+
+        System.out.println(binaryTree);
 
     }
 }
