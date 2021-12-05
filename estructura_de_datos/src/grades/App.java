cff97d0d4fa545f9ee3c51ee7fe18a4f786e28a9
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-05 14:31:22 -0700

    quick sort added

--- estructura_de_datos/src/grades/App.java
+++ estructura_de_datos/src/grades/App.java
@@ -4,7 +4,8 @@ public class App {
     public static void main(String[] args) {
         Grades grades = new Grades();
 
-        grades.sortByMergeSort(grades.grades, 0, grades.grades.length - 1);
+        // grades.sortByMergeSort(grades.grades, 0, grades.grades.length - 1);
+        grades.sortByQuickSort(grades.grades, 0, grades.grades.length - 1);
         grades.printArray(grades.grades);
     }
 }
--- estructura_de_datos/src/grades/Grades.java
+++ estructura_de_datos/src/grades/Grades.java
@@ -60,6 +60,33 @@ public class Grades {
         }
     }
 
+    public void sortByQuickSort(int A[], int izq, int der) {
+
+        int pivote=A[izq]; // tomamos primer elemento como pivote
+        int i=izq;         // i realiza la búsqueda de izquierda a derecha
+        int j=der;         // j realiza la búsqueda de derecha a izquierda
+        int aux;
+
+        while(i < j){                          // mientras no se crucen las búsquedas
+            while(A[i] <= pivote && i < j) i++; // busca elemento mayor que pivote
+            while(A[j] > pivote) j--;           // busca elemento menor que pivote
+            if (i < j) {                        // si no se han cruzado
+                aux= A[i];                      // los intercambia
+                A[i]=A[j];
+                A[j]=aux;
+            }
+        }
+
+        A[izq]=A[j];      // se coloca el pivote en su lugar de forma que tendremos
+        A[j]=pivote;      // los menores a su izquierda y los mayores a su derecha
+
+        if(izq < j-1)
+            sortByQuickSort(A,izq,j-1);          // ordenamos subarray izquierdo
+        if(j+1 < der)
+            sortByQuickSort(A,j+1,der);          // ordenamos subarray derecho
+
+    }
+
     public void printArray(int arr[]) {
         int n = arr.length;
         for (int i=0; i<n; ++i) {
