1aac48113d23bcc7273c2e1ce3e61c9695849eaa
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-05 15:50:08 -0700

    la clase Dictionary funciona como lista

--- estructura_de_datos/src/diccionario/App.java
+++ estructura_de_datos/src/diccionario/App.java
@@ -1,4 +1,19 @@
 package diccionario;
 
 public class App {
+    public static void main(String[] args) {
+        Record record1 = new Record("primera", "funciona");
+        Record record2 = new Record("segunda", "por");
+        Record record3 = new Record("tercera", "favor");
+
+        Dictionary dictionary = new Dictionary();
+
+        dictionary.insert(record1);
+        dictionary.insert(record2);
+        dictionary.insert(record3);
+
+        dictionary.showDictionary();
+
+    }
+
 }
--- estructura_de_datos/src/diccionario/Dictionary.java
+++ estructura_de_datos/src/diccionario/Dictionary.java
@@ -1,5 +1,41 @@
 package diccionario;
 
 public class Dictionary {
+    Record firstRecord;
+    Record lastRecord;
+    int length = 0;
+
+    public void insert(Record newRecord) {
+        if (this.length == 0) {
+            this.firstRecord = newRecord;
+            this.lastRecord = newRecord;
+            this.upgradeLength();
+            return;
+        }
+
+        this.lastRecord.next = newRecord;
+        this.lastRecord = newRecord;
+        this.upgradeLength();
+    }
+
+    public void showDictionary() {
+        Record currentRecord = this.firstRecord;
+
+        for(int i=0; i<this.length; i++) {
+            System.out.println("Palabra: " + currentRecord.getWord());
+            System.out.println("definicion: " + currentRecord.getDefinition());
+            System.out.println();
+
+            currentRecord = currentRecord.next;
+        }
+    }
+
+    private void upgradeLength() {
+        this.length++;
+    }
+
+    private void downgradeLength() {
+        this.length--;
+    }
 
 }
