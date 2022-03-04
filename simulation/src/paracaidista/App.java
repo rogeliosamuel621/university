e5823af9b6561abefe12552a8db1bfd82f6a275f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-03-04 11:40:36 -0700

    add chart library

--- simulation/src/paracaidista/App.java
+++ simulation/src/paracaidista/App.java
@@ -1,14 +1,27 @@
 package paracaidista;
 
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) {
-        double masa, resistencia, gravedad = 9.8;
         Scanner Keyboard = new Scanner(System.in);
+        double masa, resistencia, gravedad = 9.8;
+        double resultado;
+        final String p = "Paracaidista";
         //Introduccion de datos
 
+        //arrglo que guarda los las velocidades en las distintas posiciones.
+        double datos[] = new double[10];
+
+        //objeto que guarda los datos para posteriormente ser insertados en la grafica
+        DefaultCategoryDataset data = new DefaultCategoryDataset();
         //Peso
         do{
             System.out.println("Inserte el peso del paracaidista");
@@ -31,10 +44,34 @@ public class App{
                 +"\n-----------------------------|-----------------------------");
         //calculo de velocidad
         for(int i=0; i<10; i++){
-            System.out.println("             " + (i+1) +"               |      "
-                    +((gravedad*masa)/resistencia)*(1-Math.pow(2.7, (-(resistencia/masa)*(i+1))))
-            +"\n-----------------------------|-----------------------------");
+            resultado = ((gravedad*masa)/resistencia)*(1-Math.pow(2.7, (-(resistencia/masa)*(i+1))));
+            datos[i]=resultado;
+            if((i+1)==10){
+                System.out.println("             " + (i+1) +"              |      "+ resultado
+                        +"\n-----------------------------|-----------------------------");
+            }
+            else{
+                System.out.println("             " + (i+1) +"               |      "+ resultado
+                        +"\n-----------------------------|-----------------------------");
+            }
+
         }
+
+        //se guardan los faros en la dataset
+        for(int i=0; i<10; i++){
+            data.setValue(datos[i], p, String.valueOf(i+1));
+        }
+
+
+        JFreeChart grafico = ChartFactory.createLineChart("Paracaidista", "Distancia", "Velocidad", data);
+
+        ChartPanel panel = new ChartPanel(grafico);
+
+        JFrame ventana = new JFrame(" ");
+        ventana.setVisible(true);
+        ventana.setSize(400,200);
+
+        ventana.add(panel);
 
     }
 }
