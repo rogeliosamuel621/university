0aa165eb49fc29647ee79f6b84e42e76b581cfed
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-03-21 15:36:55 -0700

    se encapsula la simulacion en un metodo

--- metodos_numericos/src/newton_raphson/Simulation.java
+++ metodos_numericos/src/newton_raphson/Simulation.java
@@ -8,11 +8,19 @@ public class Simulation {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Calculations calculations = new Calculations();
         double x1 = getX1();
+        double errorRange = getErrorRange();
+
+        simulation(x1, errorRange);
+    }
+
+    static private String simulation(double initialValueX1, double initialValueErrorRange) {
+        Calculations calculations = new Calculations();
+        double x1 =  initialValueX1;
         double f1 = calculations.getFx(x1);
         double f2 = 0.0;
-        double errorRange = getErrorRange();
+        double errorRange = initialValueErrorRange;
+        String f2Formatted = "";
 
         int i = 0;
 
@@ -24,13 +32,15 @@ public class Simulation {
 
             NumberFormat formatter = new DecimalFormat("###.##########");
 
-            String f2Formatted = formatter.format(f2);
+            f2Formatted = formatter.format(f2);
 
             System.out.println(i + " x1: " + x1 + " F(x1): " + f1 + " F'(x1): " + f1Dx + " X2: " + x2 + " F(x2): " + f2Formatted);
 
             x1 = x2;
             f1 = f2;
         }while(Math.abs(f2) > errorRange);
+
+        return f2Formatted;
     }
 
     static private double getX1() {
