bd9aeb01aa15ebd2c90d8f27cb8858371521a151
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-03-24 16:52:14 -0700

    change functions

--- metodos_numericos/src/newton_raphson/Calculations.java
+++ metodos_numericos/src/newton_raphson/Calculations.java
@@ -2,11 +2,11 @@ package newton_raphson;
 
 public class Calculations {
     public double getFx(double x) {
-        return Math.sin(x);
+        return ((x * (Math.cos(x * x))) -4);
     }
 
     public double getFxDx(double x) {
-        return Math.cos(x);
+        return ((-2 * (x * x)) * Math.sin(x * x)) + Math.cos(x);
     }
 
     public double newtonRaphsonMethod(double x, double f1, double f1Dx) {
