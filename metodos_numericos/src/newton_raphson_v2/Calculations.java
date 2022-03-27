18c44c3b8288f03f59ea7d9faa9dad96523f3048
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-03-26 18:54:53 -0700

    v2 works with 1 solution

---
+++ metodos_numericos/src/newton_raphson_v2/Calculations.java
@@ -0,0 +1,15 @@
+package newton_raphson_v2;
+
+public class Calculations {
+    public double getFx(double x) {
+        return (x * x) - 4;
+    }
+
+    public double getFxDx(double x) {
+        return 2 * x;
+    }
+
+    public double newtonRaphsonMethod(double x, double f1, double f1Dx) {
+        return x - (f1 / f1Dx);
+    }
+}
