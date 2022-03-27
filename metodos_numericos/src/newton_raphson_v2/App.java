fc967dde9724194aa1f8bd22c5a1101702de27ee
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-03-26 23:49:07 -0700

    ya no muestra valores repetidos

--- metodos_numericos/src/newton_raphson_v2/App.java
+++ metodos_numericos/src/newton_raphson_v2/App.java
@@ -10,6 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         ArrayList<Double> resultados = new ArrayList<Double>();
+
         double puntoInicial = pedirPuntoInicial();
         double puntoFinal = pedirPuntoFinal();
         double rangoDeError = pedirRangoDeError();
@@ -19,14 +20,16 @@ public class App {
 
         while(x1 >= puntoInicial && x1 <= puntoFinal) {
 
-            double x2 = obtenerUnaSolucion(x1, rangoDeError);
+            double x2 = obtenerUnaSolucionSinOutput(x1, rangoDeError);
 
             if (esRepetido(resultados, x2) && resultados.size() != 0) {
                 System.out.println("REPETIDO");
                 double valorAnterior = resultados.get(resultados.size() - 1);
                 x1 = valorAnterior + 1;
-                x2 = obtenerUnaSolucion(x1, rangoDeError);
+                x2 = obtenerUnaSolucionSinOutput(x1, rangoDeError);
             }
+
+            obtenerUnaSolucion(x1, rangoDeError);
 
             resultados.add(x2);
             x1 = x2 + 0.5;
@@ -36,16 +39,24 @@ public class App {
             // if (i == 200) break;
             i++;
         }
-
-        double solucion = obtenerUnaSolucion(x1, rangoDeError);
-        System.out.println();
-        System.out.println("La solucion es: " + solucion);
     }
 
     static private boolean esRepetido(ArrayList<Double> arr, double x) {
         boolean esRepetido = false;
         for(int i=0; i<arr.size(); i++) {
-            if (Double.compare(arr.get(i), x) == 0) {
+
+            NumberFormat formatter = new DecimalFormat("###.##");
+            String resultadoFormateado = formatter.format(arr.get(i));
+            String xEnString = formatter.format(x);
+
+            /*
+            System.out.println();
+            System.out.println("[RESULTADO]: " + resultadoFormateado);
+            System.out.println("[X]:         " + xEnString);
+            System.out.println();
+            */
+
+            if (resultadoFormateado.equals(xEnString)) {
                 esRepetido = true;
             }
         }
@@ -73,6 +84,29 @@ public class App {
             String f2Formatted = formatter.format(f2);
 
             System.out.println(i + " x1: " + x1 + " F(x1): " + f1 + " F'(x1): " + f1Dx + " X2: " + x2 + " F(x2): " + f2Formatted);
+
+            x1 = x2;
+            f1 = f2;
+
+        }while(Math.abs(f2) > rangoDeError);
+
+        return x2;
+    }
+
+    static private double obtenerUnaSolucionSinOutput(double x1, double rangoDeError) {
+        Calculations calculations = new Calculations();
+
+        double f1 = calculations.getFx(x1);
+        double f2 = 0.0;
+        double x2;
+
+        int i = 0;
+
+        do {
+            i++;
+            double f1Dx = calculations.getFxDx(x1);
+            x2 = calculations.newtonRaphsonMethod(x1, f1, f1Dx);
+            f2 = calculations.getFx(x2);
 
             x1 = x2;
             f1 = f2;
