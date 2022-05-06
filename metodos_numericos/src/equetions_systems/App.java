4b8af5d3ed5704efecaaaa0e133b2c188536a3e1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-05-06 09:16:43 -0600

    get data correctly

--- metodos_numericos/src/equetions_systems/App.java
+++ metodos_numericos/src/equetions_systems/App.java
@@ -5,12 +5,48 @@ import java.util.Scanner;
 public class App {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Works");
+        String problem = askForProblem();
+        int numOfVariables = askForNumOfVariables();
+        int[][] equationsSystem = askForVariables(numOfVariables);
+        int errorToTolerate = askForErrorToTolerate();
+        int totalCalculations = askForTotalCalculations();
+        int initialValues = askForInitialValues();
+        int decimalFigures = askForDecimalFigures();
+
+        /*
+        System.out.println("PROBLEM: " + problem);
+        System.out.println("Numero de variables: " + numOfVariables);
+
+        System.out.println();
+        System.out.println("Sistemas de ecuaciones");
+        for (int i = 0; i < equationsSystem.length; i++) {
+            for (int j = 0; j < equationsSystem.length; j++) {
+                if (j == 0) {
+                    System.out.print(equationsSystem[i][j] + "x" + (j+1));
+                    continue;
+                }
+
+                System.out.print(" + " + equationsSystem[i][j] + "x" + (j+1));
+
+                if (j == equationsSystem.length - 1) {
+                    System.out.print(" = " + equationsSystem[i][j + 1]) ;
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Error a tolerar: " + errorToTolerate);
+        System.out.println("Total de calculos: " + totalCalculations);
+        System.out.println("Valores iniciales: " + initialValues);
+        System.out.println("Cifras decimales: " + decimalFigures);
+         */
+        
     }
 
     private static String askForProblem() {
         System.out.println("Digite el problema: ");
-        return sc.next();
+        return sc.nextLine();
     }
 
     private static int askForNumOfVariables() {
@@ -30,20 +66,21 @@ public class App {
     }
 
     private static int[][] askForVariables(int numOfVariables) {
-        int[][] equationsSystem = new int[numOfVariables][numOfVariables];
+        int[][] equationsSystem = new int[numOfVariables][numOfVariables + 1];
 
         for(int i=0; i<numOfVariables; i++) {
-            System.out.println("\n** Variables de ecuacion 1 **");
-            for (int j = 0; j < numOfVariables; j++) {
+            System.out.println("\n** Variables de ecuacion " +  (i+1) + "**");
+            for (int j = 0; j < numOfVariables + 1; j++) {
+                if (j == numOfVariables) {
+                    System.out.print("Resultado = ");
+                    int result = sc.nextInt();
+                    equationsSystem[i][j] = result;
+                    continue;
+                }
+
                 System.out.print("x" + (j+1) + " = ");
                 int value = sc.nextInt();
                 equationsSystem[i][j] = value;
-
-                if (j == numOfVariables - 1) {
-                    System.out.println("Resultado = ");
-                    int result = sc.nextInt();
-                    equationsSystem[i][j] = result;
-                }
             }
         }
 
@@ -56,7 +93,7 @@ public class App {
     }
 
     private static int askForTotalCalculations() {
-        System.out.println("Digite el error tolerante: ");
+        System.out.println("Digite el total de calculos: ");
         return sc.nextInt();
     }
 
