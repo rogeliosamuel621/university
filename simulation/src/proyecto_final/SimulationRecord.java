7d06bea783e527c0e4605611bb776d0778ff5772
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-06-06 22:27:13 -0600

    add SimulationRecord class

--- simulation/src/proyecto_final/SimulationRecord.java
+++ simulation/src/proyecto_final/SimulationRecord.java
@@ -1,5 +1,48 @@
 package proyecto_final;
 
-public class SimulationRecord {
+import java.util.ArrayList;
 
+public class SimulationRecord {
+    private final int salaryPerHour = 25; // dollars
+    private final int workHours = 8;
+    private final float salaryPerExtraHour = 37.5f; // dollars
+    private final int servicePricePerHour = 500; // dollars
+    private final int waitBusPricePerHour = 100; // dollars
+
+    private ArrayList<TableRecord> records = new ArrayList<>();
+    private int normalSalary = 0;
+    private double extraSalary = 0;
+    private double busAwaitTimePrice = 0;
+    private int servicePrice = 0;
+    private int totalCost;
+    private int teamMates;
+
+    public SimulationRecord(int teamMates) {
+        this.normalSalary = this.salaryPerHour * this.workHours * teamMates;
+        this.teamMates = teamMates;
+    }
+
+    public void updateRecord(TableRecord record) {
+        this.records.add(record);
+    }
+
+    public TableRecord getRecord(int index) {
+        return records.get(index);
+    }
+
+    public int getNormalSalary() {
+        return this.normalSalary;
+    }
+
+    public double getExtraSalary(double extraTime) {
+        return this.salaryPerExtraHour * this.extraSalary * extraTime;
+    }
+
+    public double getServicePrice(double timeService) {
+        return servicePrice * timeService;
+    }
+
+    public double getBusAwaitTimePrice(double busAwaitTime) {
+        return this.busAwaitTimePrice * busAwaitTime;
+    }
 }
