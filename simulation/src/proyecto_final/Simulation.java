4b55b4441e7a8e38f7ae5a573a040d6bbc0c980b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-06-06 22:30:10 -0600

    add Simulation

---
+++ simulation/src/proyecto_final/Simulation.java
@@ -0,0 +1,15 @@
+package proyecto_final;
+
+import java.util.Scanner;
+
+public class Simulation {
+    Scanner sc = new Scanner(System.in);
+    SimulationRecord simulationRecord3 = new SimulationRecord(3);
+    SimulationRecord simulationRecord4 = new SimulationRecord(4);
+    SimulationRecord simulationRecord5 = new SimulationRecord(5);
+    SimulationRecord simulationRecord6 = new SimulationRecord(6);
+
+
+
+
+}
